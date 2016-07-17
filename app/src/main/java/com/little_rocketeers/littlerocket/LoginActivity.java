56566cc1b87ee752744ec25ca74abb945476b439
laniley <melanie.mende@me.com>
2016-07-17 19:25:46 +0200

    close app on back press in login screen

--- app/src/main/java/com/little_rocketeers/littlerocket/LoginActivity.java
+++ app/src/main/java/com/little_rocketeers/littlerocket/LoginActivity.java
@@ -92,6 +92,14 @@ public class LoginActivity extends Activity {
     }
 
     @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(callbackManager.onActivityResult(requestCode, resultCode, data)) {
